c84385918891498de8ea63d005d6c46ba7fd9f69
kouyou <2992282886@qq.com>
2022-06-28 22:14:50 +0800

    init repository

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: yanzhourong
+ * @Date: 2022-06-27 21:57:43
+ * @LastEditTime: 2022-06-27 22:13:11
+ * @Description: 
+ */
+import React from 'react'
+import Child from './Child'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      test
+      <ul>
+        <li>111</li>
+        <li>222</li>
+      </ul>
+      <Child />
+    </div>
+  )
+}
+
---
+++ src/index.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: yanzhourong
+ * @Date: 2022-06-27 21:57:43
+ * @LastEditTime: 2022-06-27 22:07:27
+ * @Description: 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
