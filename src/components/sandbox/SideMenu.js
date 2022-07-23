81cd766de87ad9451c335dfc06d043600189ab61
kouyou <2992282886@qq.com>
2022-07-23 10:15:37 +0800

    [feature] SideMenu组件对接后台接口
    [what]SideMenu组件对接后台接口
    [why]none
    [how]see code

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,10 +1,10 @@
 /*
  * @Author: yanzhourong
  * @Date: 2022-07-18 07:11:38
- * @LastEditTime: 2022-07-20 23:32:42
+ * @LastEditTime: 2022-07-23 10:14:51
  * @Description: 
  */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import {
   AppstoreOutlined, 
@@ -14,131 +14,94 @@ import {
 } from '@ant-design/icons';
 import './index.css'
 import { Layout, Menu } from 'antd';
+import axios from 'axios';
 const { Sider } = Layout;
 
-// 模拟数组结构
-const menuList = [
-  {
-    key: "/home",
-    title: "首页",
-    icon: <UserOutlined />
-  },
-  {
-    key: "/user-manage",
-    title: "用户管理",
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: "/user-manage/list",
-        title: "用户列表",
-        icon: <UserOutlined />
-      }
-    ]
-  },
-  {
-    key: "/right-manage",
-    title: "权限管理",
-    icon: <UserOutlined />,
-    children: [
-      {
-        key: "/right-manage/role/list",
-        title: "角色列表",
-        icon: <UserOutlined />
-      },
-      {
-        key: "/right-manage/right/list",
-        title: "权限列表",
-        icon: <UserOutlined />,
-      }
-    ]
-  },
-]
-
-
-function getItem(label, key, icon, children, type) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type
-  };
-}
-
-function getItems(myList){
-  // let items = []
-  // menuList.forEach(item => {
-  //   if(item.children){
-  //     items.push(getItem(item.title, item.key, item.icon, item.children.map(subItem => {return getItem(subItem.title, subItem.key, subItem.icon)}))) 
-  //   }else{
-  //     items.push(getItem(item.title, item.key, item.icon)) 
-  //   }
-    
-  // })
-  // return items
-  // 第二种
-  // return menuList.map(item => {
-  //   if(item.children){
-  //     return getItem(item.title, item.key, item.icon, item.children.map(subItem => {return getItem(subItem.title, subItem.key, subItem.icon)}))
-  //   }else{
-  //     return getItem(item.title, item.key, item.icon)
-  //   }
-  // })
-  // 第三种，递归，可处理多个层级
-  return myList.map(item => {
-    if(item.children){
-      // return getItem(item.title, item.key, item.icon, getItems(item.children), undefined, function(){console.log("123")})
-      return getItem(item.title, item.key, item.icon, getItems(item.children))
-    }else{
-      return getItem(item.title, item.key, item.icon)
-    }
-  })
+// icon 映射表
+const iconList = {
+  '/home': <UserOutlined />,
 }
 
 
 function SideMenu(props) {
+
+  const [menu, setMenu] = useState([])
+  
+  useEffect(() => {
+    axios.get("http://localhost:5000/rights?_embed=children").then(res => {
+      setMenu(res.data)
+    })
+  }, [])
+
   const [collapsed, setCollapsed] = useState(false);
 
   const onClick = (e) => {
     props.history.push(e.key)
   }
 
-  const items = [
-    getItem('Navigation One', 'sub1', <MailOutlined />, [
-      getItem('Item 1', 'g1', null, [getItem('Option 1', '1'), getItem('Option 2', '2')], 'group'),
-      getItem('Item 2', 'g2', null, [getItem('Option 3', '3'), getItem('Option 4', '4')], 'group'),
-    ]),
-    getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-      getItem('Option 5', '5'),
-      getItem('Option 6', '6'),
-      getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-    ]),
-    getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-      getItem('Option 9', '9'),
-      getItem('Option 10', '10'),
-      getItem('Option 11', '11'),
-      getItem('Option 12', '12'),
-    ]),
-    getItem('用户管理', 'sub5', <SettingOutlined />, [
-      getItem('Option 9', '9'),
-      getItem('Option 10', '10'),
-      getItem('Option 11', '11'),
-      getItem('Option 12', '12'),
-    ]),
-  ];
+  const getItem = (label, key, icon, children, type) => {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      type
+    };
+  }
+
+  const checkPagePermission = (item) => {
+    return item.pagepermisson===1
+  }
+  
+  const getItems = (myList) => {
+    // let items = []
+    // menuList.forEach(item => {
+    //   if(item.children){
+    //     items.push(getItem(item.title, item.key, item.icon, item.children.map(subItem => {return getItem(subItem.title, subItem.key, subItem.icon)}))) 
+    //   }else{
+    //     items.push(getItem(item.title, item.key, item.icon)) 
+    //   }
+      
+    // })
+    // return items
+    // 第二种
+    // return menuList.map(item => {
+    //   if(item.children){
+    //     return getItem(item.title, item.key, item.icon, item.children.map(subItem => {return getItem(subItem.title, subItem.key, subItem.icon)}))
+    //   }else{
+    //     return getItem(item.title, item.key, item.icon)
+    //   }
+    // })
+    // 第三种，递归，可处理多个层级
+    return myList.map(item => {
+      if(item.children?.length>0 && checkPagePermission(item)){
+        // return getItem(item.title, item.key, item.icon, getItems(item.children), undefined, function(){console.log("123")})
+        return getItem(item.title, item.key, iconList[item.key], getItems(item.children))
+      }else{
+        return checkPagePermission(item) && getItem(item.title, item.key, iconList[item.key])
+      }
+    })
+  }
+
+  const selectKeys = [props.location.pathname]
+  const openKeys = ["/"+props.location.pathname.split("/")[1]]
 
   return (
     <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" >全球新闻发布管理系统</div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          items={getItems(menuList)}
-          onClick={onClick}
-          // items={items}
-        />
+        <div style={{display: "flex", height:"100%", flexDirection:"column"}}>
+          <div className="logo" >全球新闻发布管理系统</div>
+          <div style={{flex: 1, overflow: 'auto'}}>
+            <Menu
+              theme="dark"
+              mode="inline"
+              selectedKeys={selectKeys}
+              defaultOpenKeys={openKeys}
+              items={getItems(menu)}
+              onClick={onClick}
+              // items={items}
+            />
+          </div>
+        </div>
       </Sider>
   )
 }
--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,13 +1,46 @@
 /*
  * @Author: yanzhourong
  * @Date: 2022-07-18 07:27:21
- * @LastEditTime: 2022-07-18 23:00:33
+ * @LastEditTime: 2022-07-23 09:13:17
  * @Description: 
  */
 import React from 'react'
+import { Button } from 'antd'
+import axios from 'axios'
 
 export default function Home() {
+  const ajax = () => {
+    // 取数据 get
+    // axios.get("http://localhost:8000/posts").then(res => {
+    //   console.log(res.data)
+    // })
+    // 增 post
+    // axios.post("http://localhost:8000/posts", {
+    //   title: "33333",
+    //   author: "xiaoming"
+    // })
+    // 更新 put 替换式
+    // axios.put("http://localhost:8000/posts/1",{
+    //   title: "11111-修改"
+    // })
+    // 更新 patch
+    // axios.patch("http://localhost:8000/posts/1",{
+    //   title: "11111-修改"
+    // })
+    // 删除 delete
+    // axios.delete("http://localhost:8000/posts/1")
+    // _embed 向下关联
+    // axios.get("http://localhost:8000/posts?_embed=comments").then(res => {
+    //   console.log(res)
+    // })
+    // _expand 向下关联
+    axios.get("http://localhost:8000/comments?_expand=post").then(res => {
+      console.log(res)
+    })
+  }
   return (
-    <div>Home</div>
+    <div>
+      <Button onClick={ajax}>点击按钮</Button>
+    </div>
   )
 }
